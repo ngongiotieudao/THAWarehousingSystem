562cb7aac63de0e191bb27e49a123cdc6cdfa3d1
Gió Mùa Thu <nguyenminhcp221099@gmail.com>
2025-02-28 20:00:15 +0700

    fix config security

--- src/main/java/com/giomuathu/AquariumWarehousingSupportSystem/config/SecurityConfig.java
+++ src/main/java/com/giomuathu/AquariumWarehousingSupportSystem/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
